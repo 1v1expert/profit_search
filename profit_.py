e8ef25accde51c01a1c5f201c7953119811f5697
V L A D D O S <1v1expert@gmail.com>
2018-02-25 05:08:20 +0300

    Finished

--- profit_.py
+++ profit_.py
@@ -1,61 +1,59 @@
-def change_array(array):
+def array_filtering(array, array_date):
     i = 1
     while i < len(array)-1:
         if array[i-1] < array[i] < array[i+1] or array[i-1] == array[i] or array[i-1] > array[i] > array[i+1]:
             array.pop(i)
-        i += 1
-    print(array)
+            array_date.pop(i)
+        else: i += 1
     return array
 
-def procent(array, bool, reverse, logging):
+def profit(array, bool, reverse, logging):
     FIAT = ['RUR', 'USD']
     if reverse: i = int(bool)
     else: i = int(not bool)
     fiat = 1
     while i < len(array) - 1:
         if logging:
-            print(array[i + int(bool)], FIAT[int(bool)])
-            print(array[i + int(not bool)], FIAT[int(not bool)])
+            print('%s%s' % (date_array[i + int(bool)], FIAT[int(bool)]))
+            print('%s%s' % (date_array[i + int(not bool)], FIAT[int(not bool)]))
         else:
             fiat *= array[i + int(bool)]
             fiat /= array[i + int(not bool)]
-            print(array[i + int(bool)], array[i + int(not bool)])
         i += 2
     return (fiat - 1)*100
-def print_array(array, reverse):
-    if reverse:
-        usd = procent(array, False, reverse, False)
-        rur = procent(array, True, reverse, False)
+def print_array(array):
+    if array[0] > array[1]:
+        usd = profit(array, False, True, False)
+        rur = profit(array, True, True, False)
         if usd < rur: #USD < RUR
             print('Начинаем с RUR')
-            procent(array, True, reverse, True)
-            print("Прибыль: ", rur)
+            profit(array, True, True, True)
+            print("Прибыль: %d%%" % rur)
         else:
             print("Начинаем с USD")
-            procent(array, False, reverse, True)
-            print("Прибыль: ", usd)
+            profit(array, False, True, True)
+            print("Прибыль: %d%%" % usd)
     else:
-        rur = procent(array, False, reverse, False)
-        usd = procent(array, True, reverse, False)
+        rur = profit(array, False, False, False)
+        usd = profit(array, True, False, False)
         if usd < rur: #USD < RUR
             print('Начинаем с USD')
-            procent(array, False, reverse, True)
-            print("Прибыль: ", rur)
+            profit(array, False, False, True)
+            print("Прибыль: %d%%" % rur)
         else:
             print("Начинаем с RUR")
-            procent(array, True, reverse, True)
-            print("Прибыль: ", usd)
-
-def iterable(array):
-    if array[0] > array[1]:print_array(array, True)
-    else: print_array(array, False)
+            profit(array, True, False, True)
+            print("Прибыль: %d%%" % usd)
 
 
 if __name__ == '__main__':
-    A = [60, 61, 62, 58, 58, 63, 57.5]
-    B = [65, 60, 70, 62]
-    C = [70, 75, 72, 76, 69]
-    D = [70, 69]
-    new_array = change_array(D)
-    iterable(new_array)
+    date_array, array_currencies = [], []
+    line = ['','']
+    while line[0] != 'quit':
+        line = input().split('-')
+        if line[0] != 'quit':
+            date_array.append(line[0])
+            array_currencies.append(float(line[1]))
+    print_array(array_filtering(array_currencies, date_array))
+    
 
